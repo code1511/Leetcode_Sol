f7115c1ad486330478a168411b8dc02ceb05c754
Anchal Pandey <73632448+code1511@users.noreply.github.com>
2021-05-29 21:30:05 +0530

    Update SlidingWindowMaximum.cpp

--- SlidingWindow/SlidingWindowMaximum.cpp
+++ SlidingWindow/SlidingWindowMaximum.cpp
@@ -1,6 +1,31 @@
 //link of the question
 //https://leetcode.com/problems/sliding-window-maximum/submissions/
 
+//1)brute_force solution
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int>ans;
+        int n=nums.size();
+        int mx;
+        for (int i = 0; i <= n - k; i++)
+    {
+        mx = nums[i];
+ 
+        for (int j = 1; j < k; j++)
+        {
+            if (nums[i + j] > mx)
+                mx = nums[i + j];
+        }
+        ans.push_back(mx);
+    }
+        return ans;
+    }
+};
+
+
+
+//2) sliding window solution
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
