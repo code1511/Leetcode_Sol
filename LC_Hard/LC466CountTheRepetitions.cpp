81805cc2d1786584ccfaf4aeacdff7a36d18f6f6
Anchal Pandey <73632448+code1511@users.noreply.github.com>
2021-11-07 10:42:47 +0530

    Update LC466CountTheRepetitions.cpp

--- LC_Hard/LC466CountTheRepetitions.cpp
+++ LC_Hard/LC466CountTheRepetitions.cpp
@@ -22,3 +22,31 @@ public:
         return count2 / n2;
     }
 };
+
+//better approach
+ unordered_map<int, int> kToRepeatCount;
+        unordered_map<int, int> nextIndexToK;
+        kToRepeatCount[0] = 0;
+        nextIndexToK[0] = 0;
+        int j = 0, cnt = 0;
+        for (int k = 1; k <= n1; ++k) {
+            for (int i = 0; i < s1.size(); ++i) {
+                if (s1[i] == s2[j]) {
+                    ++j;
+                    if (j == s2.size()) {
+                        j = 0;
+                        ++cnt;
+                    }
+                }
+            }
+            if (nextIndexToK.find(j) != nextIndexToK.end()) {
+                int start = nextIndexToK[j];
+                int prefixCount = kToRepeatCount[start];
+                int patternCount = (n1 - start) / (k - start) * (cnt - prefixCount);
+                int suffixCount = kToRepeatCount[start + (n1 - start) % (k - start)] - prefixCount;
+                return (prefixCount + patternCount + suffixCount) / n2;
+            }
+            kToRepeatCount[k] = cnt;
+            nextIndexToK[j] = k;
+        }
+        return kToRepeatCount[n1] / n2;
